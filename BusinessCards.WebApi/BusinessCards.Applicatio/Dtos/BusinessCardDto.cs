5dd1206fb2874df30c9c41526c1966b696d850f3
tala.mohammad <tala.mohammed2000@gmail.com>
2025-10-29 22:33:37 +0300

    Initial entities, dtos and validators

---
+++ BusinessCards.WebApi/BusinessCards.Applicatio/Dtos/BusinessCardDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCards.Application.Dtos
+{
+    public record BusinessCardDto(
+    Guid Id,
+    string Name,
+    string Gender ,
+    DateTime DateOfBirth ,
+    string Email ,
+    string Phone ,
+    byte[]? Photo ,
+    string Address);
+}
--- BusinessCards.WebApi/BusinessCards.Domain/Entities/BusinessCard.cs
+++ BusinessCards.WebApi/BusinessCards.Domain/Entities/BusinessCard.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace BusinessCards.Domain.Entities
 {
-    internal class BusinessCard
+    public class BusinessCard
     {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public string Gender { get; set; }
+            public DateTime? DateOfBirth { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public byte[]? Photo { get; set; }
+            public string? Address { get; set; }
+            public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
     }
 }

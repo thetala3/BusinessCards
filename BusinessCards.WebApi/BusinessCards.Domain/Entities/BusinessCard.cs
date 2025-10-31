36d0fb9d21db108b560ad5d8a531035ef0bcf6e0
tala.mohammad <tala.mohammed2000@gmail.com>
2025-10-31 21:15:49 +0300

    API Endpoints for CRUD

--- BusinessCards.WebApi/BusinessCards.Domain/Entities/BusinessCard.cs
+++ BusinessCards.WebApi/BusinessCards.Domain/Entities/BusinessCard.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ namespace BusinessCards.Domain.Entities
 {
     public class BusinessCard
     {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
             public Guid Id { get; set; }
             public string Name { get; set; }
             public string Gender { get; set; }
---
+++ BusinessCards.WebApi/BusinessCards.WebApi/Controllers/BuisnessCardsController.cs
@@ -0,0 +1,61 @@
+﻿using BusinessCards.Application.Dtos;
+using BusinessCards.Application.Interfaces;
+using BusinessCards.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessCards.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuisnessCardsController : ControllerBase
+    {
+        private readonly ILogger<BuisnessCardsController> _logger;
+        private readonly IBusinessCardsService _businessCardsService;
+        public BuisnessCardsController(ILogger<BuisnessCardsController> logger, IBusinessCardsService businessCardsService)
+        {
+            _logger = logger;
+            _businessCardsService = businessCardsService;
+        }
+
+        [HttpGet]
+        [Route("GetAllCards")]
+        public async Task<ActionResult<List<BusinessCardRequestDto>>> GetAllBuisnessCards()
+        {
+            var cards = await _businessCardsService.GetAllCardsAsync();
+            return Ok(cards);
+        }
+
+        [HttpGet]
+        [Route("GetCard")]
+        public async Task<ActionResult<BusinessCardRequestDto>> GetCard(Guid guid) {
+            var card = await _businessCardsService.GetCard(guid);
+
+            if (card is null)
+                return NotFound(new { message = "Business card not found." });
+
+            return Ok(card);
+        }
+
+        [HttpDelete]
+        [Route("DeleteCard")]
+        public async Task<IActionResult> DeleteCard(Guid guid, CancellationToken ct)
+        {
+            if (guid == Guid.Empty) return BadRequest(new { message = "Invalid id." });
+            var deleted = await _businessCardsService.DeleteCardAsync(guid, ct);
+            if (!deleted) return NotFound();
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Route("PostCard")]
+        public async Task<IActionResult> AddCard(BusinessCardRequestDto card) {
+
+            if (card is null)
+                return BadRequest(new { message = "No cards provided." });
+
+            await _businessCardsService.CreateCard(card);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
